d7acebe61b8c8842113c0bd634251bbf01055e46
foxysatu <foxysatu232@gmail.com>
2024-04-03 21:17:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,75 @@
+import uvicorn
+from flask import Flask, render_template, redirect, request, session
+from keycloak import KeycloakOpenID
+from prometheus_flask_exporter import PrometheusMetrics
 import os
-import random
-import requests
-from opentelemetry import trace
-import uvicorn as uvicorn
-from fastapi import FastAPI, Query
-from fastapi.middleware.cors import CORSMiddleware
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.sdk.trace.export import ConsoleSpanExporter
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 
-provider = TracerProvider()
-processor = BatchSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(processor)
-trace.set_tracer_provider(
-    TracerProvider(resource=Resource.create({SERVICE_NAME: "dataManager"}))
-)
-jaeger_exporter = JaegerExporter(
-    agent_host_name=os.getenv("JAGER_HOSTNAME", "localhost"),
-    agent_port=6831,
-)
-trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(jaeger_exporter))
+app = Flask(__name__)
+app.debug = False
 
-tracer = trace.get_tracer("choice")
+app.secret_key = "SOMTHING_UNIQUE_AND_SECRET"
 
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+keycloak_openid = KeycloakOpenID(
+    server_url=os.getenv("KEYCLOAK_URL", "http://keycloakapp:8080/"),
+    client_id="ivan-client",
+    realm_name="ivan",
+    client_secret_key="1mjX90YtqTfTYHrkyYsFfBmsYD0XYgzF",
 )
 
-
-@app.get("/")
-async def root():
-    with tracer.start_as_current_span("getMovie"):
-        # r_id = random.randint(1, 1000)
-        r = requests.get("https://api.thecatapi.com/v1/images/search?limit=1")
-        return r.json()
+metrics = PrometheusMetrics(app)
 
 
-@app.get("/list/")
-async def get_list(q: list | None = Query()):
-    with tracer.start_as_current_span("getHistoryMovie"):
-        film_list = []
-        for id in q:
-            r = requests.get("https://api.thecatapi.com/v1/images/search?limit=1")
-            film_list.append(r.json())
-        return film_list
+@app.route('/', methods=["POST", "GET"])
+def index():
+    if session.get("valid", False):
+        return render_template('index.html')
+    if request.method == "POST":
+        print(request.form)
+        username = str(request.form["fname"])
+        password = str(request.form["lname"])
+        # Get Token
+        try:
+            token = keycloak_openid.token(username, password)
+            userinfo = keycloak_openid.userinfo(token["access_token"])
+            app.logger.info(f"Userinfo: {userinfo}")
+            token_info = keycloak_openid.introspect(token["access_token"])
+            print(token_info)
+            app.logger.info(f"Userroles: {token_info['realm_access']['roles']}")
+            if "movie" not in token_info["realm_access"]["roles"]:
+                return render_template(
+                    "login.html",
+                    wrong_datg_visability="collapse",
+                    no_permission_visability="visible",
+                )
+            else:
+                session["valid"] = True
+                return render_template("index.html")
+        except Exception as e:
+            app.logger.error(e)
+            return render_template(
+                "login.html",
+                wrong_datg_visability="visible",
+                no_permission_visability="collapse",
+            )
+
+    return render_template(
+        "login.html",
+        wrong_datg_visability="collapse",
+        no_permission_visability="collapse",
+    )
+
+
+@app.route('/all')
+def fetch_all():
+    return render_template('all.html')
+
+
+@app.route('/logout')
+def logout():
+    session["valid"] = False
+    return redirect("/")
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('PORT', 80)))
+    uvicorn.run(host="0.0.0.0", port=int(os.getenv('PORT', 5051)))
+
